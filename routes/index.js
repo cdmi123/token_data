b365f3ef05d31163a686e912ff8e2bf3616085d6
cdmi123 <languagepdf@gmail.com>
2023-05-27 11:21:04 +0530

    demo

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var { register, get_data , check_token} = require('../controller/usercontroller'
 
 router.post('/',register);
 
-router.get('/get_data',check_token , get_data);
+router.get('/get_data' , get_data);
 
 
 module.exports = router;
